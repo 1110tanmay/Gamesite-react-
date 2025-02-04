2f555c549e6bd3b84ef1fd31e8955ad5c84c6db6
1110tanmay <1110tanmay@gmail.com>
2025-02-04 16:23:13 -0700

    Dynamically adding items.

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,17 @@
 function ListGroup() {
+  let items = ["New York", "London", "Paris", "Mumbai", "Tokyo"];
+  items = [];
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>Hello World!</h1>
+      {items.length === 0 && <p>Hi there!</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
